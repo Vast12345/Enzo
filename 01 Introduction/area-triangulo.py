f567518de45e7068b4577d7b08ba03823a239f33
sborrero09 <sborrero09@gmail.com>
2023-09-20 21:39:49 -0500

    feat: :tada: algorithm
    Introduction to a basic algorithm

---
+++ 01 Introduction/area-triangulo.py
@@ -0,0 +1,9 @@
+# Calcular el area de un tirangulo
+base = float(input("Input the base of the triangle: "))
+altura = float(input("Input the height of the triangle: "))
+
+area = (base * altura) / 2
+# print("The area of the triangle is", area)
+print(f"The area of the triangle is: {area}")
+
+
