d24ac5e86fc4f61ce1f34e76e23bbae70a4ffb92
sborrero09 <sborrero09@gmail.com>
2023-09-20 21:38:30 -0500

    feat: :tada: dataTypes
    Introdoction to basic data types

---
+++ 01 Introduction/tipos-datos.py
@@ -0,0 +1,13 @@
+# Edad tipo
+edad = 25
+print("edad:", type(edad))
+edad = "daniel" #ahora es cadena
+print("nombre:", type(edad))
+
+# Tipo Float
+temp = 33.5
+print("temp:", type(temp))
+
+# Tipo logico
+soltero = True # False
+print("soltero:", type(soltero))
