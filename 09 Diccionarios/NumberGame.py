4617850abb64ce6c94c6da7d0d186594e9389924
Vast12345 <vastprogramming@gmail.com>
2023-10-09 15:44:25 -0500

    feat: :tada: Oct 9 Exercise
    An exercise that requires you to use dictionaries, while and for loops, and sorting commands as well as imports.

---
+++ 09 Diccionarios/NumberGame.py
@@ -0,0 +1,61 @@
+import random
+
+def readStr():
+    while True:
+        try:
+            phrase = input("Tell me your name: ")
+            phrase = phrase.strip()
+            if len(phrase) == 0 or phrase.isalnum() == False:
+                print("Error: Invalid input, must be a string")
+                input("Press any key to continue")
+                print("\n", "=" * 30, "\n")
+                continue
+            return phrase
+        except Exception as e:
+            print(f"Error: Invalid input, must be a string\n{e}")
+            input("Press any key to continue")
+            print("\n", "=" * 30, "\n")
+
+number = random.randint(1, 10)
+
+def readNum():
+    while True:
+        try:
+            num = int(input("Enter a number between 1 and 1000: "))
+            if num < 1 or num > 1000:
+                print("Error: Number must be between 1 and 1000")
+                input("Press any key to continue")
+                print("\n", "=" * 30, "\n")
+                continue
+            return num
+        except ValueError:
+            print("Error: Invalid input, must be an integer")
+            input("Press any key to continue")
+            print("\n", "=" * 30, "\n")
+WinnerList = {}
+while True:
+    number = random.randint(1, 10)
+    name = readStr()
+    for i in range(1, 11):
+        print(f"Tries: {i}/10")
+        n = readNum()
+        if n < number:
+            print("FAIL: Your number is too small")
+        elif n > number:
+            print("FAIL: Your number is too big")
+        elif n == number:
+            print("Ganaste")
+            WinnerList[name] = i
+            break
+    number = random.randint(1, 10)
+            
+
+
+    option = input("Do you wish to continue? (Y/N) ")
+    if option.lower() == "n":
+        break
+    elif option.lower() == "y":
+        continue
+
+sorted_winners = sorted(WinnerList.items(),key=lambda x: x[1])
+print(sorted_winners)           
