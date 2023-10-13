3441518a06eee3e38ec887ffe5c6f16b66029742
Vast12345 <vastprogramming@gmail.com>
2023-10-13 15:48:17 -0500

    feat: :sparkles: Added Options
    Added the modifcation option for the Oct 13 Exercise. Missing the List employees option to be able to finish the program.

--- 11- Archivos/Json/datos-persona.py
+++ 11- Archivos/Json/datos-persona.py
@@ -1,5 +1,86 @@
 import json
 
+def readId():
+    while True:
+        try:
+            num = int(input("Enter your ID: "))
+            if num < 1:
+                print("Error: Invalid input, must be a positive integer")
+                input("Press any key to continue")
+                print("\n", "=" * 30, "\n")
+                continue
+            return num
+        except ValueError:
+            print("Error: Invalid input, must be an integer")
+            input("Press any key to continue")
+            print("\n", "=" * 30, "\n")
+
+def readEdad():
+    while True:
+        try:
+            num = int(input("Enter your age: "))
+            if num < 1:
+                print("Error: Invalid input, must be a positive integer")
+                input("Press any key to continue")
+                print("\n", "=" * 30, "\n")
+                continue
+            return num
+        except ValueError:
+            print("Error: Invalid input, must be an integer")
+            input("Press any key to continue")
+            print("\n", "=" * 30, "\n")
+
+def readSexo():
+    while True:
+        try:
+            phrase = input("Input gender: ")
+            phrase = phrase.strip()
+            if len(phrase) == 0 or phrase.isalnum() == False:
+                print("Error: Invalid input, must be a string")
+                input("Press any key to continue")
+                print("\n", "=" * 30, "\n")
+                continue
+            elif phrase.lower() != "m" and phrase.lower() != "f":
+                print("Gender must be either (M/F)")
+                input("Press any key to continue")
+                continue
+            return phrase
+        except Exception as e:
+            print(f"Error: Invalid input, must be a string\n{e}")
+            input("Press any key to continue")
+            print("\n", "=" * 30, "\n")
+
+def readTelefono():
+    while True:
+        try:
+            num = int(input("Enter your phone number: "))
+            if num < 1:
+                print("Error: Invalid input, must be a positive integer")
+                input("Press any key to continue")
+                print("\n", "=" * 30, "\n")
+                continue
+            return num
+        except ValueError:
+            print("Error: Invalid input, must be an integer")
+            input("Press any key to continue")
+            print("\n", "=" * 30, "\n")
+
+def readStr():
+    while True:
+        try:
+            phrase = input("Input the name of the Personal: ")
+            phrase = phrase.strip()
+            if len(phrase) == 0 or phrase.isalnum() == False:
+                print("Error: Invalid input, must be a string")
+                input("Press any key to continue")
+                print("\n", "=" * 30, "\n")
+                continue
+            return phrase
+        except Exception as e:
+            print(f"Error: Invalid input, must be a string\n{e}")
+            input("Press any key to continue")
+            print("\n", "=" * 30, "\n")
+
 def guardarEmpleado(lstPersonal, ruta):
     try:
         fd = open(ruta, "w")
@@ -25,7 +106,7 @@ def existeId(id, lstPersonal):
 def borrarPersonal(lstPersonal, ruta):
     print("\n\n3. Borrar Personal")
 
-    id = int(input("Input ID: "))
+    id = readId()
     if not existeId(id, lstPersonal):
         print("No existe un empleado cpn ese ID")
         input()
@@ -43,22 +124,98 @@ def borrarPersonal(lstPersonal, ruta):
     else:
         print("Ocurrio un error al borrar el empleado")
 
+def modifyPersonal(lstPersonal, ruta):
+    id = readId()
+    if existeId(str(id), lstPersonal):
+        print("That ID does not exist")
+        input()
+        return
+    while True:
+            print("*** Modify Perosnal ***".center(40))
+            print("1- Edit name")
+            print("2- Edit age")
+            print("3- Edit gender")
+            print("4- Edit phone number")
+            print("5- Return")
+            n = int(input("Choose which option you would like to modify: "))
+            if n < 1 or n > 5:
+                print("Error: Input must be between 1 and 5")
+                input("Press any key to continue")
+                print("\n", "=" * 30, "\n")
+                continue
+            break
+    if n == 1:
+        for i in range(len(lstPersonal)):
+            datos = lstPersonal[i]
+            k = int(list(datos.keys())[0])
+            if k == id:
+                lstPersonal[i][str(id)]["nombre"] = readStr()
+        if guardarEmpleado(lstPersonal, ruta) == True:
+            print("The employee has been modified successfully")
+            input()
+        else:
+            print("Ocurrio un error al borrar el empleado")
+
+    elif n == 2:
+        for i in range(len(lstPersonal)):
+            datos = lstPersonal[i]
+            
+            k = int(list(datos.keys())[0])
+            if k == id:
+                
+                lstPersonal[i][str(id)]["edad"] = readEdad()
+        if guardarEmpleado(lstPersonal, ruta) == True:
+            print("The employee has been modified successfully")
+            input()
+        else:
+            print("Ocurrio un error al borrar el empleado")
+    elif n == 3:
+        for i in range(len(lstPersonal)):
+            datos = lstPersonal[i]
+            
+            k = int(list(datos.keys())[0])
+            if k == id:
+                
+                lstPersonal[i][str(id)]["sexo"] = readSexo()
+        if guardarEmpleado(lstPersonal, ruta) == True:
+            print("The employee has been modified successfully")
+            input()
+        else:
+            print("Ocurrio un error al borrar el empleado")
+    elif n == 4:
+        for i in range(len(lstPersonal)):
+            datos = lstPersonal[i]
+            
+            k = int(list(datos.keys())[0])
+            if k == id:
+                
+                lstPersonal[i][str(id)]["telefono"] = readTelefono()
+        if guardarEmpleado(lstPersonal, ruta) == True:
+            print("The employee has been modified successfully")
+            input()
+        else:
+            print("Ocurrio un error al borrar el empleado")
+
+
+
+        
+
 def agregarPersonal(lstPersonal, ruta):
     print("\n\n1. Agregar Personal")
 
-    id = int(input("Input the ID: "))
+    id = readId()
     while existeId(id , lstPersonal):
         print("==> Ya existe un empleado con ese ID")
         input()
-        id = int(input("\nInput the ID: "))
+        id = readId()
 
-    nombre = input("Name: ")
-    edad = int(input("Age: "))
-    sexo = input("Sexo (M/F): ")
-    telefono = int(input("Telefono: "))
+    nombre = readStr()
+    edad = readEdad()
+    sexo = readSexo()
+    telefono = readTelefono()
 
     dicEmpleado = {}
-    dicEmpleado[id] = {"nombre":nombre, "edad":edad, "sexo":sexo, "telefono":telefono}
+    dicEmpleado[str(id)] = {"nombre":nombre, "edad":edad, "sexo":sexo, "telefono":telefono}
     lstPersonal.append(dicEmpleado)
 
     if guardarEmpleado(lstPersonal, ruta) == True:
@@ -119,12 +276,14 @@ rutaFile = "11- Archivos/Json/datpersonal.json"
 lstPersonal = []
 lstPersonal = cargarInfo(lstPersonal, rutaFile)
 while True:
+
     option = menu()
     if option == 1:
         agregarPersonal(lstPersonal, rutaFile)
+        print(lstPersonal)
         pass
     elif option == 2:
-        pass
+        modifyPersonal(lstPersonal, rutaFile)
     elif option == 3:
         borrarPersonal(lstPersonal, rutaFile)
     elif option == 4:
